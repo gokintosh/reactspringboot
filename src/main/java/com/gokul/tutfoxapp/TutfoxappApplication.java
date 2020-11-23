f6088678f3f3525e3ba99d32e87aec327e425316
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-23 23:06:00 +0100

    initial commit

---
+++ src/main/java/com/gokul/tutfoxapp/TutfoxappApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.tutfoxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutfoxappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutfoxappApplication.class, args);
+    }
+
+}
